308d45effeae109451ab86b3b98daa446b4def78
SuperKkong <160845066+SuperKkong@users.noreply.github.com>
2026-03-30 13:36:06 +0700

    lab13

---
+++ Lab13/myproject/myapp/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Person
+from django.db.models import Q  # ✅ เพิ่ม
+
+# แสดงข้อมูล + ค้นหา
+def index(request):
+    persons = Person.objects.all()  # ของเดิม
+
+    query = request.GET.get('q')  # ✅ เพิ่ม
+
+    if query:
+        persons = persons.filter(   # ✅ filter ทับของเดิม
+            Q(name__icontains=query) |
+            Q(id__icontains=query)
+        )
+
+    return render(request, 'index.html', {
+        'persons': persons,
+        'query': query
+    })
+
+
+# เพิ่มข้อมูล
+def form(request):
+
+    if request.method == "POST":
+        name = request.POST['name']
+        age = request.POST['age']
+
+        Person.objects.create(
+            name=name,
+            age=age
+        )
+
+        return redirect('/')
+
+    return render(request, 'form.html')
+
+
+# แก้ไขข้อมูล
+def edit(request,id):
+
+    person = get_object_or_404(Person,id=id)
+
+    if request.method == "POST":
+
+        person.name = request.POST['name']
+        person.age = request.POST['age']
+
+        person.save()
+
+        return redirect('/')
+
+    return render(request,'edit.html',{
+        'person':person
+    })
+
+
+# ลบข้อมูล
+def delete(request,id):
+
+    person = Person.objects.get(id=id)
+    person.delete()
+
+    return redirect('/')
